5df7badb8bb76579db1aabcdb5536ed97d5c6f57
ShivamSIngh317 <shivamsingh93103@gmail.com>
2025-04-10 11:44:14 +0530

    Add files via upload

---
+++ arrmean.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float sum = 0.0, mean;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    float arr[n]; // Variable-length array (C99 and above)
+
+    printf("Enter %d numbers:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%f", &arr[i]);
+        sum += arr[i];
+    }
+
+    mean = sum / n;
+    printf("Mean = %.2f\n", mean);
+
+    return 0;
+}
