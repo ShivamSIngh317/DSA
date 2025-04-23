c963b05cefd8c63ee8691150fe909f50af2ecfc1
ShivamSIngh317 <shivamsingh93103@gmail.com>
2025-04-23 20:49:09 +0530

    dllreverse.c

--- dllreverse.c
+++ dllreverse.c
@@ -3,74 +3,53 @@
 
 struct Node {
     int data;
-    struct Node* prev;
     struct Node* next;
 };
 
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
-    newNode->prev = NULL;
     newNode->next = NULL;
     return newNode;
 }
 
-void displayList(struct Node* head) {
+void printList(struct Node* head) {
     struct Node* temp = head;
-    printf("Doubly Linked List: ");
     while (temp != NULL) {
-        printf("%d <-> ", temp->data);
+        printf("%d -> ", temp->data);
         temp = temp->next;
     }
     printf("NULL\n");
 }
 
-struct Node* reverseList(struct Node* head) {
-    struct Node* temp = NULL;
-    struct Node* current = head;
+void reverseList(struct Node** head_ref) {
+    struct Node* prev = NULL;
+    struct Node* ptr = *head_ref;
+    struct Node* next = NULL;
 
-    while (current != NULL) {
-        temp = current->prev;
-        current->prev = current->next;
-        current->next = temp;
-        current = current->prev;
+    while (ptr != NULL) {
+        next = ptr->next;
+        ptr->next = prev;
+        prev = ptr;
+        ptr = next;
     }
 
-    if (temp != NULL) {
-        head = temp->prev;
-    }
-
-    return head;
+    *head_ref = prev;
 }
 
 int main() {
-    struct Node* head = NULL;
-    struct Node* tail = NULL;
-    int n, value;
+    struct Node* head = createNode(1);
+    head->next = createNode(2);
+    head->next->next = createNode(3);
+    head->next->next->next = createNode(4);
 
-    printf("How many nodes do you want to create? ");
-    scanf("%d", &n);
+    printf("Original List:\n");
+    printList(head);
 
-    for (int i = 0; i < n; i++) {
-        printf("Enter data for node %d: ", i + 1);
-        scanf("%d", &value);
-        struct Node* newNode = createNode(value);
-        if (head == NULL) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            tail->next = newNode;
-            newNode->prev = tail;
-            tail = newNode;
-        }
-    }
+    reverseList(&head);
 
-    displayList(head);
-
-    head = reverseList(head);
-
-    printf("Reversed ");
-    displayList(head);
+    printf("Reversed List:\n");
+    printList(head);
 
     return 0;
 }
